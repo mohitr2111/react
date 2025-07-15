fd120ab25bd7474cd9c9466c74e9fa4c48a495e4
mohitr2111 <mohitranjan2111@gmail.com>
2025-07-15 22:40:07 +0530

    experiment

--- 01_vite-project/src/main.jsx
+++ 01_vite-project/src/main.jsx
@@ -2,8 +2,52 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
+import React from 'react'
+
+// method 2
+function MyApp()
+{
+  return(
+    <>
+    <div>
+      <h1>Custom app</h1>
+    </div>
+    <App/>
+    </>
+  )
+}
+
+// ***WRONG METHOD***
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://www.google.com/',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+// METHOD 3
+const otherElement = (
+  <a href='https://www.google.com' target="main">Click to google</a>
+)
+
+
+// method 4 
+const anotherElement = React.createElement(
+  'a',
+  {
+    href:'https://www.google.com',
+    target: 'blank'
+  },
+  'click will direct to google'
+
+)
 
 createRoot(document.getElementById('root')).render(
   
     <App />
+    // <MyApp/>
+    // otherElement
+    // anotherElement
 )
